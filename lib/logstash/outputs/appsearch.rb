92e4e57540501afdf027867c16cb74849ef3597a
João Duarte <jsvd@users.noreply.github.com>
2018-10-23 10:01:45 +0100

    modifications from code review (#2)
    * docs: added note about document_id reuse causing document overwrite
    * docs: added api_key documentation
    * docs: fixed required fields flag for host, api_key and engine
    * docs: removed leftover text
    * renamed store_timestamp to timestamp_destination
    * test connection to app search during register and report errors
    * docs: improve documentation regarding fields started with "@"

--- lib/logstash/outputs/appsearch.rb
+++ lib/logstash/outputs/appsearch.rb
@@ -10,12 +10,21 @@ class LogStash::Outputs::AppSearch < LogStash::Outputs::Base
   config :engine, :validate => :string, :required => true
   config :host, :validate => :string, :required => true
   config :api_key, :validate => :password, :required => true
-  config :store_timestamp, :validate => :string
+  config :timestamp_destination, :validate => :string
   config :document_id, :validate => :string
 
   public
   def register
     @client = Client.new(@host, @api_key.value)
+    @client.get_engine(@engine)
+  rescue => e
+    if e.message =~ /401/
+      raise ::LogStash::ConfigurationError.new("Failed to connect to App Search. Error: 401. Please check your credentials")
+    elsif e.message =~ /404/
+      raise ::LogStash::ConfigurationError.new("Failed to connect to App Search. Error: 404. Please check if host '#{@host}' is correct and you've created an engine with name '#{@engine}'")
+    else
+      raise ::LogStash::ConfigurationError.new("Failed to connect to App Search. #{e.message}")
+    end
   end
 
   public
@@ -36,8 +45,8 @@ class LogStash::Outputs::AppSearch < LogStash::Outputs::Base
       doc = event.to_hash
       # we need to remove default fields that start with "@"
       # since appsearch doesn't accept them
-      if @store_timestamp
-        doc[@store_timestamp] = doc.delete("@timestamp")
+      if @timestamp_destination
+        doc[@timestamp_destination] = doc.delete("@timestamp")
       else # delete it
         doc.delete("@timestamp")
       end
